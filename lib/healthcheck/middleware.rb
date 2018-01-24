55f886d70b0851cac16fae60e29fa3a5c9920a62
Alex Martin <atm@gracehill.com>
2018-01-24 10:45:47 -0500

    Shush the logs so AWS doesn't spam us.

--- lib/healthcheck/middleware.rb
+++ lib/healthcheck/middleware.rb
@@ -5,7 +5,9 @@ module Healthcheck
     end
 
     def call(env)
-      return [200, {}, []] if env['PATH_INFO'] == '/healthcheck'
+      Rails.logger.silence do
+        return [200, {}, []] if env['PATH_INFO'] == '/healthcheck'
+      end
       @app.call env
     end
   end
